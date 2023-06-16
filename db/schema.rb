5d65f624cdea31c1ab5e6f474fc2d344120e1363
Shweta Mishra <shweta.aith@gmail.com>
2023-06-16 22:53:32 +0530

    Add user validations

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,14 @@
 ActiveRecord::Schema[7.0].define(version: 2023_06_16_105225) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
